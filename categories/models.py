1ec30c4eaef4ac0f1a6a353fc3c01b140c095350
Rafaelkrc <rafaelkrc@hotmail.com>
2024-08-29 17:24:24 +0100

    criado tabela de subcategorias e seus template
    close #8

--- categories/models.py
+++ categories/models.py
@@ -3,9 +3,22 @@ from django.db import models
 
 class Category(models.Model):
     TYPE_CHOICE = (('E', 'Entrada'), ('S', 'Saída'), ('T', 'Transferência'),)
-    name = models.CharField(max_length=15)
+    name = models.CharField(max_length=30)
     type = models.CharField(max_length=1, choices=TYPE_CHOICE, blank=False, null=False)
     active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
+
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default='S', related_name='subcategory_category')
+    name = models.CharField(max_length=30)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    def category_name(self):
+        return self.category.name
+    category_name.name = 'Category'
--- categories/views.py
+++ categories/views.py
@@ -39,3 +39,41 @@ class CategoryDeleteView(DeleteView):
     model = models.Category
     template_name = 'category_delete.html'
     success_url = reverse_lazy('category_list')
+
+
+class SubCategoryListView(ListView):
+    model = models.SubCategory
+    template_name = 'subcategory_list.html'
+    context_object_name = 'subcategories'
+
+    def get_queryset(self):
+        queryset = super().get_queryset().order_by('name')
+        name = self.request.GET.get('name')
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
+
+
+class SubCategoryCreateView(CreateView):
+    model = models.SubCategory
+    template_name = 'subcategory_create.html'
+    form_class = forms.SubCategoryForm
+    success_url = reverse_lazy('subcategory_list')
+
+
+class SubCategoryDetailView(DetailView):
+    model = models.SubCategory
+    template_name = 'subcategory_detail.html'
+
+
+class SubCategoryUpdateView(UpdateView):
+    model = models.SubCategory
+    template_name = 'subcategory_update.html'
+    form_class = forms.SubCategoryForm
+    success_url = reverse_lazy('subcategory_list')
+
+
+class SubCategoryDeleteView(DeleteView):
+    model = models.SubCategory
+    template_name = 'subcategory_delete.html'
+    success_url = reverse_lazy('subcategory_list')
