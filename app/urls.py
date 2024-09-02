7fa802e659f84b61ee37bf7ba31112f3963dae04
Rafaelkrc <rafaelkrc@hotmail.com>
2024-09-02 16:10:19 +0100

    criado tabela de lançamento de contas e template de listar as despesas
    close #9

--- app/urls.py
+++ app/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('account_postings.urls')),
     path('', include('bank_account.urls')),
     path('', include('base.urls')),
     path('', include('categories.urls')),
