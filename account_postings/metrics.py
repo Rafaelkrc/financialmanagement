249097426addfb25f604e1f93333187222cca1e9
Rafaelkrc <rafaelkrc@hotmail.com>
2024-09-05 17:55:19 +0100

    criado a lógica de alteração de um laçamento alterar o saldo atual do caixa.
    close #17

--- account_postings/metrics.py
+++ account_postings/metrics.py
@@ -14,3 +14,11 @@ def add_bank_balance_on_credit(bank_account, credit_value):
     """
     bank_account.current_balance += credit_value
     bank_account.save(update_fields=['current_balance'], update_from_posting=True)
+
+
+def update_bank_balance(bank_account, debit_value):
+    """
+    Atualiza o saldo do banco após um lançamento de débito.
+    """
+    bank_account.current_balance -= debit_value
+    bank_account.save(update_fields=['current_balance'], update_from_posting=True)
