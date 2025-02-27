e24717122b65e291658be06e02ed2d3f274244c3
Rafaelkrc <rafaelkrc@hotmail.com>
2025-02-27 15:36:35 +0000

    create transfer between accounts
    close #21

--- account_postings/metrics.py
+++ account_postings/metrics.py
@@ -1,5 +1,3 @@
-
-
 def add_bank_balance_on_debit(bank_account, debit_value):
     """
     Atualiza o saldo do banco após um lançamento de débito.
@@ -18,7 +16,7 @@ def add_bank_balance_on_credit(bank_account, credit_value):
 
 def update_bank_balance(bank_account, debit_value):
     """
-    Atualiza o saldo do banco após um lançamento de débito.
+    Atualiza o saldo do banco após uma alteração de lançamento de débitoou crédito.
     """
     bank_account.current_balance -= debit_value
     bank_account.save(update_fields=['current_balance'], update_from_posting=True)
--- account_postings/models.py
+++ account_postings/models.py
@@ -1,4 +1,5 @@
-from django.db import models
+from django.db import models, transaction
+from django.utils import timezone
 from categories.models import Category, SubCategory
 from bank_account.models import BankAccount
 from account_postings.metrics import add_bank_balance_on_debit, add_bank_balance_on_credit, update_bank_balance
@@ -14,6 +15,10 @@ class AccountPosting(models.Model):
     expiry_date = models.DateField()
     debit_value = models.DecimalField(max_digits=20, decimal_places=2, default=0)
     credit_value = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    transfer_pair = models.OneToOneField(
+        "self", on_delete=models.SET_NULL, null=True, blank=True, related_name="paired_transfer"
+    )
+    transfering = models.BooleanField(default=False)
 
     def save(self, *args, **kwargs):
         # Verifica se o lançamento já existe no banco de dados
@@ -28,21 +33,77 @@ class AccountPosting(models.Model):
             if original.credit_value != self.credit_value:
                 update_bank_balance(self.bank, original.credit_value)
 
-        if self.credit_value < 0:
-            self.credit_value = abs(self.credit_value)  # Garante que o valor seja positivo
-        super().save(*args, **kwargs)
-
+        self.credit_value = abs(self.credit_value)  # Garante que o valor seja positivo
         if self.debit_value > 0:
             self.debit_value = -abs(self.debit_value)  # Garante que o valor seja negativo
 
         super().save(*args, **kwargs)
 
+        # Atualiza apenas os campos do par, sem chamar save() diretamente
+        if self.transfer_pair:
+            AccountPosting.objects.filter(pk=self.transfer_pair.pk).update(
+                debit_value=-self.credit_value,
+                credit_value=-self.debit_value
+            )
+
         # Atualiza o saldo com os novos valores
-        if self.debit_value != 0:
-            add_bank_balance_on_debit(self.bank, self.debit_value)
-        elif self.credit_value != 0:
-            add_bank_balance_on_credit(self.bank, self.credit_value)
-        super().save(*args, **kwargs)
+        if not self.transfering:
+            if self.debit_value != 0:
+                add_bank_balance_on_debit(self.bank, self.debit_value)
+            elif self.credit_value != 0:
+                add_bank_balance_on_credit(self.bank, self.credit_value)
+
+    @classmethod
+    def transfer(cls, from_bank, to_bank, value, description, issue_date, expiry_date, category, subcategory):
+        if from_bank == to_bank:
+            raise ValueError("A conta de origem e destino não podem ser a mesma.")
+
+        if value <= 0:
+            raise ValueError("O valor da transferência deve ser maior que zero.")
+
+        with transaction.atomic():  # Gera os dois lançamentos simultaneos
+
+            category, _ = Category.objects.get_or_create(name="Transferência")
+            subcategory, _ = SubCategory.objects.get_or_create(name="Transferência")
+            # Criar o lançamento de débito
+            debit_posting = cls.objects.create(
+                bank=from_bank,
+                debit_value=-abs(value),
+                credit_value=0,
+                description=f"Transferência para {to_bank.name}: {description}",
+                issue_date=timezone.now(),
+                expiry_date=timezone.now(),
+                category=Category.objects.get(name="Transferência"),
+                subcategory=SubCategory.objects.get(name="Transferência"),
+                transfering=True,
+            )
+
+            # Criar o lançamento de crédito
+            credit_posting = cls.objects.create(
+                bank=to_bank,
+                debit_value=0,
+                credit_value=abs(value),
+                description=f"Transferência de {from_bank.name}: {description}",
+                issue_date=timezone.now(),
+                expiry_date=timezone.now(),
+                category=Category.objects.get(name="Transferência"),
+                subcategory=SubCategory.objects.get(name="Transferência"),
+                transfering=True,
+            )
+
+            # Atualizar os dois objetos para criar o vínculo
+            debit_posting.transfer_pair = credit_posting
+            credit_posting.transfer_pair = debit_posting
+            # Apenas atualizamos os campos necessários, sem chamar `save()` novamente
+            AccountPosting.objects.filter(pk=debit_posting.pk).update(transfer_pair=credit_posting)
+            AccountPosting.objects.filter(pk=credit_posting.pk).update(transfer_pair=debit_posting)
+
+            # Atualiza saldos das contas
+            if debit_posting.transfering and credit_posting.transfering:
+                add_bank_balance_on_debit(from_bank, -abs(value))
+                add_bank_balance_on_credit(to_bank, abs(value))
+
+        return debit_posting, credit_posting
 
     def delete(self, *args, **kwargs):
         # Reverte o impacto do lançamento no saldo da conta bancária antes de deletá-lo
@@ -50,6 +111,16 @@ class AccountPosting(models.Model):
             update_bank_balance(self.bank, self.debit_value)  # Reverte o débito
         elif self.credit_value != 0:
             update_bank_balance(self.bank, self.credit_value)  # Reverte o crédito
+
+        if self.transfer_pair:
+            # Salva a referência antes de excluir o primeiro objeto
+            pair = self.transfer_pair
+            self.transfer_pair = None
+            self.save(update_fields=['transfer_pair'])  # Remove a referência antes da exclusão
+
+            # Exclui o lançamento vinculado
+            pair.transfer_pair = None  # Remove a referência do outro lançamento
+            pair.delete()
 
         super().delete(*args, **kwargs)  # Deleta o lançamento
 
