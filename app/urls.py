69023ce717f30c455d73f14567aa9f294df17ebe
Rafaelkrc <rafaelkrc@hotmail.com>
2025-10-06 16:16:54 +0100

    create login and logout of user.

--- app/urls.py
+++ app/urls.py
@@ -17,11 +17,14 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('account_postings.urls')),
     path('', include('bank_account.urls')),
     path('', include('base.urls')),
